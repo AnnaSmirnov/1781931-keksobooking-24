1c66b7d1941227fd8e4a43f3d1f3b2230d6618e7
AnnaSmirnov <mantyhora@gmail.com>
2021-10-25 11:41:22 +0300

    Отрисуй меня полностью (часть 1)

--- js/data.js
+++ js/data.js
@@ -1,6 +1,10 @@
-import {getRandomInteger, getRandomFloat, getRandomItemsArray} from './utils.js';
-
-const INTEGER_PRICE = getRandomInteger(0, 100000);
+const TITLES = [
+  'Роскошный дворец',
+  'Уютная квартирка',
+  'Большой,тёплый дом',
+  'Простенькое бунгало',
+  'Комфортабельный отель',
+];
 
 const TYPES_HOUSING = [
   'palace',
@@ -10,25 +14,17 @@ const TYPES_HOUSING = [
   'hotel',
 ];
 
-const TYPE_HOUSING_NUMBER = TYPES_HOUSING[getRandomInteger(0, 4)];
-
-const NUMBER_ROOMS = getRandomInteger(1, 10);
-
-const NUMBER_GUESTS = getRandomInteger(1, 10);
-
 const CHECKIN_TIME = [
   '12:00',
   '13:00',
   '14:00',
 ];
-const CHECKIN_TIME_NUMBER = CHECKIN_TIME[getRandomInteger(0, 2)];
 
 const CHECKOUT_TIME = [
   '12:00',
   '13:00',
   '14:00',
 ];
-const CHECKOUT_TIME_NUMBER = CHECKOUT_TIME[getRandomInteger(0, 2)];
 
 const FEATURES_LIST = [
   'wifi',
@@ -38,65 +34,17 @@ const FEATURES_LIST = [
   'elevator',
   'conditioner',
 ];
-const createRandomFeatures = () => getRandomItemsArray(FEATURES_LIST);
+
+const DESCRIPTION = [
+  'Здесь есть все для комфортного проживания.',
+  'Просто и со вкусом',
+  'Шикарней места не найти!',
+];
 
 const PHOTOS_LIST = [
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
 ];
-const createRandomPhotos = () => getRandomItemsArray(PHOTOS_LIST);
 
-const LAT_NUMBER = getRandomFloat(35.65, 35.7, 5);
-
-const LNG_NUMBER = getRandomFloat(139.7, 139.8, 5);
-
-const OFFER_ADDRESS = `${LAT_NUMBER}, ${LNG_NUMBER}`;
-
-const CHOSEN_HOUSING_COUNT = 10;
-
-// Функция создания автора объявления
-
-const createAuthor = () => {
-  const avatarNumber = getRandomInteger(1, 10);
-  if (avatarNumber >= 10) {
-    return {avatar: `img/avatars/user${avatarNumber}.png`};
-  }
-  return {avatar: `img/avatars/user0${avatarNumber}.png`};
-};
-
-// Функция создания координат
-
-const createLocation = () => ({
-  lat: LAT_NUMBER,
-  lng: LNG_NUMBER,
-});
-
-// Функция создания объявления
-
-const createOffer = () => ({
-  title: 'Давайте выберем идеальное жилье!',
-  address: OFFER_ADDRESS,
-  price: INTEGER_PRICE,
-  type: TYPE_HOUSING_NUMBER,
-  rooms: NUMBER_ROOMS,
-  guests: NUMBER_GUESTS,
-  checkin: CHECKIN_TIME_NUMBER,
-  checkout: CHECKOUT_TIME_NUMBER,
-  features: createRandomFeatures(),
-  description: 'Здесь есть все для комфортного проживания.',
-  photos: createRandomPhotos(),
-});
-
-const createChosenHousing = () =>
-  ({
-    author: createAuthor (),
-    offer: createOffer(),
-    location: createLocation(),
-  });
-
-const createAnnouncement = () => {
-  Array.from({length: CHOSEN_HOUSING_COUNT}, createChosenHousing);
-};
-
-export {createAnnouncement};
+export {TITLES,TYPES_HOUSING,CHECKIN_TIME,CHECKOUT_TIME,FEATURES_LIST,DESCRIPTION,PHOTOS_LIST};
---
+++ js/popup.js
@@ -0,0 +1,148 @@
+import {getRandomInteger, getRandomFloat, getRandomItemsArray,getRandomArrayElement} from './utils.js';
+import {TITLES,TYPES_HOUSING,CHECKIN_TIME,CHECKOUT_TIME,FEATURES_LIST,DESCRIPTION,PHOTOS_LIST} from './data.js';
+
+const ADVERT_COUNT = 10;
+
+const createAdvert =(index) => {
+  const LAT_NUMBER = getRandomFloat(35.65, 35.7, 5);
+  const LNG_NUMBER = getRandomFloat(139.7, 139.8, 5);
+  const createAvatar = () => {
+    const avatarNumber = getRandomInteger(1, ADVERT_COUNT);
+    if (avatarNumber >= 10) {
+      return {avatar: `img/avatars/user${avatarNumber}.png`};
+    }
+    return {avatar: `img/avatars/user0${avatarNumber}.png`};
+  };
+
+  return {
+    author: {
+      avatar: createAvatar(),
+    },
+    offer: {
+      title: getRandomArrayElement(TITLES),
+      address: `${LAT_NUMBER}, ${LNG_NUMBER}`,
+      price: getRandomInteger(0, 100000),
+      type: getRandomArrayElement(TYPES_HOUSING),
+      rooms: getRandomInteger(1, 10),
+      guests: getRandomInteger(1, 10),
+      checkin: getRandomArrayElement(CHECKIN_TIME),
+      checkout: getRandomArrayElement(CHECKOUT_TIME),
+      features: getRandomItemsArray(FEATURES_LIST),
+      description: getRandomArrayElement(DESCRIPTION),
+      photos: getRandomItemsArray(PHOTOS_LIST),
+    },
+    location: {
+      lat: LAT_NUMBER,
+      lng: LNG_NUMBER,
+    },
+  };
+};
+Array.from({length: ADVERT_COUNT}, createAdvert);
+
+
+const houseType = {
+  flat: 'Квартира ',
+  bungalow: 'Бунгало',
+  house: 'Дом',
+  palace: 'Дворец',
+  hotel: 'Отель',
+};
+
+const advertTemplate = document.querySelector('#card').content.querySelector('.popup');
+const advertListFragment = document.createDocumentFragment();
+
+const advertListElement = document.querySelector('#map-canvas');
+
+
+const generateAdvert = (advert) => {
+  const advertCard = advertTemplate.cloneNode(true);
+  advertTemplate.appenedChild(advertCard);
+
+  const title = advert.offer.title;
+  const titleElement = advertCard.querySelector('.popup__title');
+  if (title) {
+    titleElement.textContent = title;
+  } else {
+    titleElement.remove();
+  }
+
+  const address = advert.offer.address;
+  const addressElement = advertCard.querySelector('.popup__text--address');
+  if (address) {
+    addressElement.textContent = address;
+  } else {
+    addressElement.remove();
+  }
+
+  const price = advert.offer.price;
+  const priceElement = advertCard.querySelector('.popup__text--price');
+  if (price) {
+    priceElement.textContent = `${price} ₽/ночь`;
+  } else {
+    priceElement.remove();
+  }
+
+  const type = houseType[advert.offer.type];
+  const typeElement = advertCard.querySelector('.popup__type');
+  if (type) {
+    typeElement.textContent = type;
+  } else {
+    typeElement.remove();
+  }
+
+  const rooms = advert.offer.rooms;
+  const guests = advert.offer.guests;
+  const capacityElement = advertCard.querySelector('.popup__text--capacity');
+  if (rooms && guests) {
+    capacityElement.textContent = `${rooms} комнаты для ${guests} гостей`;
+  } else {
+    capacityElement.remove();
+  }
+
+  const checkin = advert.offer.checkin;
+  const checkout = advert.offer.checkout;
+  const timeElement = advertCard.querySelector('.popup__text--time');
+  if (checkin && checkout) {
+    timeElement.textContent = `Заезд после ${checkin}, выезд до ${checkout}`;
+  } else {
+    timeElement.remove();
+  }
+
+  const features = advert.offer.features;
+  const featuresElement = advertCard.querySelector('.popup__features');
+  if (features) {
+    featuresElement.textContent = features;
+  } else {
+    featuresElement.remove();
+  }
+
+  const description = advert.offer.description;
+  const descriptionElement = advertCard.querySelector('.popup__description');
+  if (description) {
+    descriptionElement.textContent = description;
+  } else {
+    descriptionElement.remove();
+  }
+
+  const photos = advert.offer.photos;
+  const photosElement = advertCard.querySelector('.popup__photos');
+  if(photos) {
+    photosElement.src = photos;
+  } else {
+    photosElement.remove();
+  }
+
+  const avatar = advert.author.avatar;
+  const avatarElement = advertCard.querySelector('.popup__avatar');
+  if(avatar) {
+    avatarElement.src = photos;
+  } else {
+    avatarElement.remove();
+  }
+
+  advertListFragment.appendChild(advertCard);
+
+  advertListElement.appendChild(advertListFragment);
+};
+
+export {generateAdvert};
